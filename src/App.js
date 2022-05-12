9e00ebd623e6004f1f00f0d9246616049bf55e35
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-13 01:08:52 +0600

    navbar daysiui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn btn-primary">button</button>
+    <div className="">
+      <Navbar></Navbar>
     </div>
   );
 }
